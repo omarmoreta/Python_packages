621d0c8c7bb26ee48f1215b2be100cef2f8183be
Omar <o.moreta@gmail.com>
2022-04-29 19:53:33 -0400

    created python packages

---
+++ math_package/math_module.py
@@ -0,0 +1,11 @@
+def add(*args):
+    sum = 0
+    for int in args:
+        sum += int
+    return sum
+
+def multiply(*args):
+    total = 1
+    for int in args:
+        total *= int
+    return total
---
+++ script.py
@@ -0,0 +1,4 @@
+from math_package.math_module import add, multiply
+import math_package.sub_package.math_helper
+
+print(multiply(add(1,2), add(3,4)))
